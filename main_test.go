73f0e3b6e97cf178a9a2237b4e2990ffe66d913d
xKub5oNx <61093867+kubajaru@users.noreply.github.com>
2025-07-26 16:41:48 +0200

    Build pipeline

--- main_test.go
+++ main_test.go
@@ -6,39 +6,12 @@ import (
 	"testing"
 )
 
-// Real server, not httptest
-func startTestServer() *http.Server {
-	mux := setupRouter()
-	srv := &http.Server{Addr: ":0", Handler: mux} // :0 tells OS to assign a free port
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			panic(err)
-		}
-	}()
-
-	return srv
-}
-
 // Test using httptest
 func TestTasksIntegration(t *testing.T) {
 	server := httptest.NewServer(setupRouter())
 	defer server.Close()
 
 	resp, err := http.Get(server.URL + "/tasks")
-	if err != nil {
-		t.Fatalf("Failed to send GET request: %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		t.Errorf("Got non ok status code: %d", resp.StatusCode)
-	}
-}
-
-func TestRealServerTasksIntegration(t *testing.T) {
-	server := startTestServer()
-
-	resp, err := http.Get("http://" + server.Addr + "/tasks")
 	if err != nil {
 		t.Fatalf("Failed to send GET request: %v", err)
 	}
